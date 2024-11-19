30b4e03540a75867754f9ae919613d911e7d9381
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-11-19 17:31:21 +0530

    Day 02

---
+++ 08_Funcitons_Basics.py
@@ -0,0 +1,23 @@
+def hello():
+    print("Hello")
+
+hello()
+print(type(hello))
+
+
+def add(a:int, b:int)->int:
+    return a+b
+
+print(add(1, 2))
+print(add(1.1, 2.2))
+print(add("Test", "String"))
+
+class Test:
+    pass
+
+t1 = Test()
+t2 = Test()
+
+# add(t1, t2)
+
+
