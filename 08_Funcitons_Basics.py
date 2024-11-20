c5e0b8dd71a32fc7aae464b37be25516536794ee
Ramakant Debata <ramakant.s.debata@gmail.com>
2024-11-20 12:26:28 +0530

    Day 03

--- 08_Funcitons_Basics.py
+++ 08_Funcitons_Basics.py
@@ -97,87 +97,122 @@
 
 #region Variable args
 
-lst = [1, 2]
-a = lst[0]
-b = lst[1]
+# lst = [1, 2]
+# a = lst[0]
+# b = lst[1]
 
-p, q = lst      # Implicit Unpacking
+# p, q = lst      # Implicit Unpacking
 
+# # lst = [1, 2, 3]
+# # a, b = lst
+
+# # lst = [ [1, 2] ]
+# # a, b = lst
+
+
+# ## Unpacking in the arg list, while making a call
+# def add(a, b):
+#     return a + b
+
+# lst = [1, 2]
+
+# print(add(*lst))            # ' *' prepended to a collection in the CALL unpacks the collection
+
+
+
+# # Packing 
 # lst = [1, 2, 3]
-# a, b = lst
+# a, _, b = lst
+# print(a, b)
 
-# lst = [ [1, 2] ]
-# a, b = lst
-
-
-## Unpacking in the arg list, while making a call
-def add(a, b):
-    return a + b
-
-lst = [1, 2]
-
-print(add(*lst))            # ' *' prepended to a collection in the CALL unpacks the collection
+# # for _ in range(5):
+# #     print("Hi")
 
 
+# lst = [1, 2, 3, 4]
+# a, *ignore, b = lst
 
-# Packing 
-lst = [1, 2, 3]
-a, _, b = lst
-print(a, b)
+# print(a, b)
+# print(type(ignore), ignore)
 
-# for _ in range(5):
-#     print("Hi")
-
-
-lst = [1, 2, 3, 4]
-a, *ignore, b = lst
-
-print(a, b)
-print(type(ignore), ignore)
-
-a, *_, b = lst
+# a, *_, b = lst
 
 
-lst = [1, 2, 3, 4, 5, 6, 7]
-# a, *ig1, c, *ig2, b = lst   # <-- Error: Only one permitted
+
+# # # st = {1, 2, 3, 4}
+# # # a, *ignore, b = st
+# # # print(ignore)
+# # # print(a, b)
 
 
-def add(a, b, *data):         # Variable arg list
-    print(type(data), data)
-    sum = a + b
-    for val in data:
-        sum += val
 
-    return sum
-
-# print(add())
-# print(add(1))
-print(add(1, 2))
-print(add(1, 2, 3))
-print(add(1, 2, 3, 4))
-print(add(1, 2, 3, 4, 5))
-
-# (Pos, Kw, VarArg, KwVarArgs)
-
-def PrintEmp(ProjMgr, AccMgr, *names, **emps):
-    print(f"{ProjMgr=}, {AccMgr=}")
-    print(type(names), f"{names=}")
-    print(type(emps), f"{emps=}")
-    for name in emps.values():
-        print(name)
-
-PrintEmp("Manish", "Rakesh")
-PrintEmp(ProjMgr="Manish", AccMgr="Rakesh")
-PrintEmp(ProjMgr="Manish", AccMgr="Rakesh", SalesHead="Abhijeet")
-PrintEmp("Manish", "Rakesh", "Abhijeet")
-PrintEmp("Manish", AccMgr="Rakesh", SalesHead="Abhijeet")
+# lst = [1, 2, 3, 4, 5, 6, 7]
+# # a, *ig1, c, *ig2, b = lst   # <-- Error: Only one permitted
 
 
-def Func(*args, **kwArgs):
-    print(f"{args=}", f"{kwArgs=}")
+# def add(a, b, *data):         # Variable arg list
+#     print(type(data), data)
+#     sum = a + b
+#     for val in data:
+#         sum += val
 
-Func()
-Func(1, 2)
-Func(1, 2, x=3)
+#     return sum
+
+# # print(add())
+# # print(add(1))
+# print(add(1, 2))
+# print(add(1, 2, 3))
+# print(add(1, 2, 3, 4))
+# print(add(1, 2, 3, 4, 5))
+
+# # (Pos, Kw, VarArg, KwVarArgs)
+
+# def PrintEmp(ProjMgr, AccMgr, *names, **emps):
+#     print(f"{ProjMgr=}, {AccMgr=}")
+#     print(type(names), f"{names=}")
+#     print(type(emps), f"{emps=}")
+#     for name in emps.values():
+#         print(name)
+
+# PrintEmp("Manish", "Rakesh")
+# PrintEmp(ProjMgr="Manish", AccMgr="Rakesh")
+# PrintEmp(ProjMgr="Manish", AccMgr="Rakesh", SalesHead="Abhijeet")
+# PrintEmp("Manish", "Rakesh", "Abhijeet")
+# PrintEmp("Manish", AccMgr="Rakesh", SalesHead="Abhijeet")
+
+
+# def Func(*args, **kwArgs):
+#     print(f"{args=}", f"{kwArgs=}")
+
+# Func()
+# Func(1, 2)
+# Func(1, 2, x=3)
+
+
+#endregion
+
+#region Recursion
+
+num = 5
+res = 5 * 4 * 3 * 2 * 1
+
+def FactIterative(num):
+    prod = 1
+    for i in range(1, num+1):
+        prod *= i
+    return prod
+
+def FactRec(num):
+
+    # Termination condition
+    if num == 1:
+        return 1
+    
+    prod = num * FactRec(num-1)
+    return prod
+
+# print(FactIterative(num))
+
+print(FactRec(num))
 
 #endregion
